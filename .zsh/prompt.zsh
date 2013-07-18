8a0121dbaee032cafcbb1ba631e97b8df185829e
Stephen Ball <sdball@gmail.com>
2013-07-17 22:29:47 -0400

    Customize prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,12 +1,8 @@
 function git_prompt_info {
   local ref=$(git symbolic-ref HEAD 2> /dev/null)
   local gitst="$(git status 2> /dev/null)"
-  local pairname=${${${GIT_AUTHOR_EMAIL#pair+}%@github.com}//+/\/}
-  if [[ ${pairname} == 'ch' || ${pairname} == '' ]]; then
-    pairname=''
-  else
-    pairname=" ($pairname)"
-  fi
+  local branch="$(git branch 2> /dev/null | grep '*' | sed 's/* //')"
+  local statuscolor="green"
 
   if [[ -f .git/MERGE_HEAD ]]; then
     if [[ ${gitst} =~ "unmerged" ]]; then
@@ -14,19 +10,18 @@ function git_prompt_info {
     else
       gitstatus=" %{$fg[green]%}merged%{$reset_color%}"
     fi
-  elif [[ ${gitst} =~ "Changes to be committed" ]]; then
-    gitstatus=" %{$fg[blue]%}!%{$reset_color%}"
-  elif [[ ${gitst} =~ "use \"git add" ]]; then
-    gitstatus=" %{$fg[red]%}!%{$reset_color%}"
+  elif [[ ${gitst} =~ "Changes" || ${gitst} =~ "Untracked" ]]; then
+    statuscolor="yellow"
   elif [[ -n `git checkout HEAD 2> /dev/null | grep ahead` ]]; then
-    gitstatus=" %{$fg[yellow]%}*%{$reset_color%}"
+    statuscolor="yellow"
   else
     gitstatus=''
   fi
 
   if [[ -n $ref ]]; then
-    echo "%{$fg_bold[green]%}/${ref#refs/heads/}%{$reset_color%}$gitstatus$pairname"
+    echo "%{$fg[$statuscolor]%}[${ref#refs/heads/}]$gitstatus${reset_color}"
   fi
 }
 
-PROMPT='%~%<< $(git_prompt_info)${PR_BOLD_WHITE}>%{${reset_color}%} '
+PROMPT='$(PWD) $(git_prompt_info)
+> '
