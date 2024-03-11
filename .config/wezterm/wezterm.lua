794598573a7891925d52ad778230c76d89827d68
Stephen Ball <sdball@gmail.com>
2024-03-11 11:51:33 -0400

    Enable wezterm tab bar

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -27,6 +27,6 @@ return {
 
   -- Appearance
   window_background_opacity   	= 0.9  	, -- [1.0] alpha channel value with floating point numbers in the range 0.0 (meaning completely translucent/transparent) through to 1.0 (meaning completely opaque)
-  enable_tab_bar              	= false	, -- [true]
+  enable_tab_bar              	= true	, -- [true]
   hide_tab_bar_if_only_one_tab	= false	, -- [false] hide the tab bar when there is only a single tab in the window
 }
